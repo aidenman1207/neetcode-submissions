eab737198230e856d3ae577f280c9d0b9c6bb99b
aidenman1207 <aiden.wang.yang@gmail.com>
2026-09-13 16:51:44 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int count;
+        for (int i = 0; i < nums.length; i++) {
+            count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+                if (count > 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
